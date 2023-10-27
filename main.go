a6cd8522f1a1c29b9d0f3fe8dde243ca1e27f912
Meresa Gebrewahd <mera@Meresas-MBP.attlocal.net>
2023-10-27 12:12:45 -0700

    function modified

--- main.go
+++ main.go
@@ -1,17 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 func main() {
-	
+
 	msg, err := hello_graohite("my message")
 	if err != nil {
-		fmt.Println("Error")
+		fmt.Println("Error: ", err)
 		return
 	}
 	fmt.Println(msg)
 }
 
 func hello_graohite(msg string) (string, error) {
+
+	if len(msg) < 10 {
+		return msg, errors.New("few msg content")
+	}
+
 	return msg, nil
 }
