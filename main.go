3bf2a2b0c047137cd41c90baf11fb640adb45876
Meresa Gebrewahd <mera@Meresas-MBP.attlocal.net>
2023-10-27 12:07:23 -0700

    remove .idea folder

--- main.go
+++ main.go
@@ -3,5 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hello graphite")
+	
+	msg, err := hello_graohite("my message")
+	if err != nil {
+		fmt.Println("Error")
+		return
+	}
+	fmt.Println(msg)
+}
+
+func hello_graohite(msg string) (string, error) {
+	return msg, nil
 }
